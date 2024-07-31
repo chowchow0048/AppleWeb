7bbabcd0063a409b1cb98db373615743150a8887
chowchow0048 <suess98@naver.com>
2024-07-31 16:50:56 +0900

    reset prod.py

--- appleWeb/config/settings/prod.py
+++ appleWeb/config/settings/prod.py
@@ -1,24 +1,3 @@
-import environ
-
 from .base import *
 
-ALLOWED_HOSTS = ["13.124.187.120", "banpo-apple.com", "www.banpo-apple.com"]
-STATIC_ROOT = BASE_DIR / "static/"
-STATICFILES_DIRS = []
-DEBUG = False
-
-env = environ.Env()
-environ.Env.read_env(BASE_DIR / ".env")
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": "5432",
-    }
-}
-
-CSRF_TRUSTED_ORIGINS = ["https://banpo-apple.com", "https://www.banpo-apple.com"]
+ALLOWED_HOSTS = ["3.37.147.68"]
