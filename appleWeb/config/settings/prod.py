02552096d365601346b9ac8af7bfd27eaa7ca8ae
chowchow0048 <suess98@naver.com>
2024-07-31 20:31:30 +0900

    yes www?

--- appleWeb/config/settings/prod.py
+++ appleWeb/config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ["3.37.147.68", "banpo-apple.com"]
+ALLOWED_HOSTS = ["3.37.147.68", "banpo-apple.com", "www.banpo-apple.com"]
 STATIC_ROOT = BASE_DIR / "static/"
 STATICFILES_DIRS = []
 
