5959f4485b83d009cab391857ffb93114ad8a415
chowchow0048 <suess98@naver.com>
2024-08-01 10:56:03 +0900

    prod.py updated for postfreSQL

--- appleWeb/config/settings/prod.py
+++ appleWeb/config/settings/prod.py
@@ -1,3 +1,5 @@
+import environ
+
 from .base import *
 
 ALLOWED_HOSTS = ["3.37.147.68", "banpo-apple.com", "www.banpo-apple.com"]
@@ -5,3 +7,17 @@ STATIC_ROOT = BASE_DIR / "static/"
 STATICFILES_DIRS = []
 
 DEBUG = False
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / ".env")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": "5432",
+    }
+}
