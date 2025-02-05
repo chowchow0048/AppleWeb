74d960ba788ac0417e1fa7a9278867f5579d5b8e
chowchow0048 <suess98@naver.com>
2025-02-05 14:26:36 +0900

    main notice now lists articles from latest to last

--- appleWeb/main/views.py
+++ appleWeb/main/views.py
@@ -5,7 +5,7 @@ from common.models import Board, Review
 
 # main_home
 def main(request):
-    article_list = Board.objects.all()
+    article_list = Board.objects.all().order_by("-created_at")
     paginator = Paginator(article_list, 10)
 
     page = request.GET.get("page")
@@ -30,7 +30,7 @@ def main(request):
 
 # main_home의 공지사항
 def notice_data(request):
-    article_list = Board.objects.all().order_by("-created_at")
+    article_list = Board.objects.all().order_by("created_at")
     paginator = Paginator(article_list, 15)
 
     page = request.GET.get("page", 1)
@@ -54,35 +54,3 @@ def notice_detail(request, id):
 def review_detail(request, id):
     review = get_object_or_404(Review, pk=id)
     return render(request, "main/main_review_detail.html", {"review": review})
-
-
-# def notice(request):
-#     articles = Board.objects.all()
-#     return render(request, "main/main_notice.html", {"articles": articles})
-
-
-# from django.http import JsonResponse
-# from django.contrib.auth import authenticate, login
-# from common.forms import SignUpForm
-
-# def ajax_login(request):
-#     username = request.POST.get("username")
-#     password = request.POST.get("password")
-#     user = authenticate(request, username=username, password=password)
-#     if user is not None:
-#         login(request, user)
-#         return JsonResponse({"success": True})
-#     else:
-#         return JsonResponse({"success": False, "error": "Authentication failed"})
-
-# def signup_view(request):
-#     if request.method == "POST":
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             # 적절한 리다이렉트 또는 추가 로직 처리
-#             return redirect("main")
-#     else:
-#         form = SignUpForm()
-
-#     return render(request, "main/signup.html", {"form": form})
