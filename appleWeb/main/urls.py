bacb8f53bcff8a91a7a57d75a3fc35b30a581e17
chowchow0048 <suess98@naver.com>
2025-06-19 12:20:41 +0900

    asdf

--- appleWeb/main/urls.py
+++ appleWeb/main/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path("notice/<int:id>", views.notice_detail, name="notice_detail"),
     path("review/<int:id>", views.review_detail, name="review_detail"),
     path("notice_data/", views.notice_data, name="notice_data"),
-    # path("ajax/login/", views.ajax_login, name="ajax_login"), # AJAX 요청을 처리할 URL 추가
-    # path("get-coordinates/", views.get_coordinates, name="get_coordinates"),
 ]
